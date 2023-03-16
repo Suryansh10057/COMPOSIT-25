45cb93e3c8383272542da04e2bd25d97a9dda32b
Ujjawal Kumar <ujjawalrr@gmail.com>
2023-03-16 23:19:32 +0530

    dev tools disabled

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { AuthContextProvider } from "./context/AuthContext";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 ReactDOM.render(
     <>
