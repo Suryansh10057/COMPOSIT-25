67342d433ba4383391c30c986df96197561c0cf4
Ujjawal Kumar <ujjawalrr@gmail.com>
2023-03-29 02:05:24 +0530

    accommodation

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,19 +9,40 @@ import Footer from '../Common/Footer';
 import FunFact from '../Common/FunFact';
 import lax from 'lax.js';
 import Partner from '../Common/Partner';
- 
+import './Popup/PopupMsg.css'
+import { Link } from 'react-router-dom';
+
 class Home extends React.Component {
     constructor(props) {
         super(props)
         lax.setup()
-    
-        document.addEventListener('scroll', function(x) {
+
+        document.addEventListener('scroll', function (x) {
             lax.update(window.scrollY)
         }, false)
-    
+
         lax.update(window.scrollY)
     }
-    render(){
+    render() {
+        function openForm() {
+            document.getElementById("popupForm").style.display = "block";
+        }
+        function closeForm() {
+            document.getElementById("popupForm").style.display = "none";
+        }
+        window.onclick = function (event) {
+            let modal = document.getElementById('loginPopup');
+            let popupBtn = document.getElementById('popupBtn');
+            if (event.target !== modal) {
+                closeForm();
+            }
+            if (event.target === popupBtn) {
+                openForm();
+            }
+        }
+        window.onload = function () {
+            openForm();
+        }
         return (
             <React.Fragment>
                 {/* Main Banner */}
@@ -38,7 +59,13 @@ class Home extends React.Component {
                 <Partner />
                 {/* Speakers Area */}
                 <Speakers />
-
+                <div className="loginPopup" id='loginPopup'>
+                    <div className="formPopup2" id="popupForm">
+                        <p className='popupMsg2'>Accommodation facility is available for people attending the fest offline at IIT KHARAGPUR.</p>
+                        <p className='popupMsg2'>Participation in any event is free of cost, however if you need accommodation, you need to pay Rs.300 in Cash, for dormitory room.</p>
+                        {/* <Link to="/login" className='popupLink2'>Login now to register for event</Link> */}
+                    </div>
+                </div>
                 <Footer />
 
                 {/* Back Top top */}
@@ -47,5 +74,5 @@ class Home extends React.Component {
         );
     }
 }
- 
+
 export default Home;
