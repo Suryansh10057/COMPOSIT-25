4e11f08ad001cb3d32b8b1159033145bf51f6510
Krishna Chaudhari <116093539+Krishnapc04@users.noreply.github.com>
2025-02-06 11:24:51 +0530

    hosted backend added

--- src/const.jsx
+++ src/const.jsx
@@ -1,5 +1,7 @@
 // const BaseUrl = "http://localhost:5000";
-const BaseUrl = "https://composit-25-server.onrender.com"
+// const BaseUrl = "https://composit-25-server.onrender.com"
+const BaseUrl = "https://api.composit.in";
+
 
 
 export default BaseUrl;
