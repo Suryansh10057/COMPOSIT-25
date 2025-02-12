dd35e02d16bd07f05937fd3b287a71c8a1ebe43b
Krishna Chaudhari <116093539+Krishnapc04@users.noreply.github.com>
2025-02-12 17:03:56 +0530

    hosted api added

--- src/const.jsx
+++ src/const.jsx
@@ -1,6 +1,6 @@
-const BaseUrl = "http://localhost:5000";
+// const BaseUrl = "http://localhost:5000";
 // const BaseUrl = "https://composit-25-server.onrender.com"
-// const BaseUrl = "https://api.composit.in";
+const BaseUrl = "https://api.composit.in";
 
 
 
